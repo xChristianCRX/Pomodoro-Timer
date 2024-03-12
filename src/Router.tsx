90e23577c0641cb6e1b05f9225f83142dd39e78a
Christian Ricci <riccichristian338@gmail.com>
2024-03-12 10:58:44 -0300

    FEAT: History index and styles added

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route} from "react-router-dom"
 
 import { Home } from "./pages/Home";
-import { History } from "./pages/History/history";
+import { History } from "./pages/History";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 
 export function Router(){
---
+++ src/pages/History/index.tsx
@@ -0,0 +1,47 @@
+import { HistoryContainer, HistoryList, Status } from "./styles";
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td><Status statusColor="green">Concluído</Status></td>
+          </tbody>
+          <tbody>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td><Status statusColor="green">Concluído</Status></td>
+          </tbody>
+          <tbody>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td><Status statusColor="yellow">Andamento</Status></td>
+          </tbody>
+          <tbody>
+            <td>Tarefa</td>
+            <td>20 minutos</td>
+            <td>Há 2 meses</td>
+            <td><Status statusColor="red">Interrompido</Status></td>
+          </tbody>
+        </table>
+      </HistoryList>
+
+    </HistoryContainer>
+  )
+}
