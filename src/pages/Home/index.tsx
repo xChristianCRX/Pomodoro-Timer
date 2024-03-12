5388d438736d41abacb03e6eb8bffc632d4a23b4
Christian Ricci <riccichristian338@gmail.com>
2024-03-12 20:20:34 -0300

    FEAT: Hook Zod added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,39 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StarCountdownButton, TaskInput } from "./styles";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as zod from "zod"
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser no mínimo de 5 minutos")
+    .max(60, "O ciclo precisa ser no máximo de 60 minutos")
+})
 
 export function Home() {
+  const { register, handleSubmit, watch} = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  const task = watch("task")
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" list="task-suggestions" placeholder="Dê um nome para o seu projeto"/>
+          <TaskInput 
+            id="task" 
+            list="task-suggestions" 
+            placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
+          />
 
           <datalist id="task-suggestions">
             <option value="Projeto 1"></option>
@@ -22,6 +48,7 @@ export function Home() {
             id="minutesAmount" 
             placeholder="00"
             max={60}
+            {...register("minutesAmount", {valueAsNumber:true})}
           />
 
           <span>minutos.</span>
@@ -35,7 +62,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StarCountdownButton type="submit">
+        <StarCountdownButton type="submit" disabled={!task}>
           <Play size={24} />
           Começar
         </StarCountdownButton>
